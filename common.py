a99cb55e34ee9a32989958b0e2c13352c8d2b730
monosabbath <mono.sabbath@gmail.com>
2025-10-17 16:52:18 +0200

    Update max_new_tokens and adjust penalties

--- common.py
+++ common.py
@@ -7,7 +7,7 @@ class GenerateRequest(BaseModel):
     prompt: str
     vocab_lang: Literal["en", "es"]
     vocab_n_words: int
-    max_new_tokens: int = 100
+    max_new_tokens: int = 518
     num_beams: int = 10
-    repetition_penalty: float = 1.5
-    length_penalty: float = 2.0
+    repetition_penalty: float = 1.0
+    length_penalty: float = 1.0
