4653cea66878c6c192189427ccdc4bb88eaef29a
monosabbath <mono.sabbath@gmail.com>
2025-11-14 05:58:48 +0100

    Add sampling parameters to model configuration

--- wordlist_generation/api/routers/models.py
+++ wordlist_generation/api/routers/models.py
@@ -15,3 +15,9 @@ class ChatCompletionRequest(BaseModel):
     vocab_n_words: Optional[int] = None
     num_beams: Optional[int] = 5
     length_penalty: Optional[float] = 1.0
+
+    # Sampling parameters
+    temperature: Optional[float] = 1.0
+    top_p: Optional[float] = 1.0
+    top_k: Optional[int] = 50
+    repetition_penalty: Optional[float] = 1.0
